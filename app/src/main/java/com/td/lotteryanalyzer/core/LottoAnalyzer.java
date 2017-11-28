d1882e894e01e0b8fec3983b5c5c968f6c3b64e1
Yu Che <cheyu0926@gmail.com>
2017-11-28 04:01:11 -0800

    First commit

---
+++ app/src/main/java/com/td/lotteryanalyzer/core/LottoAnalyzer.java
@@ -0,0 +1,15 @@
+package com.td.lotteryanalyzer.core;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.td.lotteryanalyzer.R;
+
+public class LottoAnalyzer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lotto_analyzer);
+    }
+}
